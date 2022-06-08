19a9f368322ff5518dda9223eb68362abccea095
Victor Cruz <victor.cruz@globant.com>
2022-06-08 09:47:52 -0500

    Initial project

---
+++ pokedex-swiftui/pokedex-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pokedex-swiftui
+//
+//  Created by Victor Cruz Galindo on 08/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pokedex-swiftui/pokedex-swiftui/pokedex_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedex_swiftuiApp.swift
+//  pokedex-swiftui
+//
+//  Created by Victor Cruz Galindo on 08/06/22.
+//
+
+import SwiftUI
+
+@main
+struct pokedex_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
